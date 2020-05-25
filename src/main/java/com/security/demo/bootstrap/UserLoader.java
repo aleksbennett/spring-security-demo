1550b415da3c4631d96041887e3ab8bf90e420a2
Bennett <Aleks.Bennett@bregroup.com>
2020-05-25 16:59:59 +0100

    Altered UserLoader to use the UserService instead of repository

--- src/main/java/com/security/demo/bootstrap/UserLoader.java
+++ src/main/java/com/security/demo/bootstrap/UserLoader.java
@@ -1,8 +1,9 @@
 package com.security.demo.bootstrap;
 
-import com.security.demo.model.User;
-import com.security.demo.repository.UserRepository;
+import com.security.demo.dto.UserRegistrationDto;
+import com.security.demo.service.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,24 +13,36 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserLoader implements CommandLineRunner {
     
-    private final UserRepository userRepository;
+    @Autowired
+    private UserService userService;
 
     @Override
     public void run(String... args) throws Exception {
-        if( userRepository.count() == 0 ){
+        if( userService.count() == 0 ){
             loadUserObjects();
         }
         
-        System.out.println("User count: " + userRepository.count());
+        System.out.println("User count: " + userService.count());
     }
 
     public void loadUserObjects(){
-        userRepository.save(
-            User.builder()
+        userService.save(UserRegistrationDto.builder()
             .email("alekstest1@test.com")
+            .confirmEmail("alekstest1@test.com")
             .firstName("Aleks")
             .lastName("Test")
-            .password("Test 1")
+            .password("mypass")
+            .confirmPassword("mypass")
+            .build()
+        );
+
+        userService.save(UserRegistrationDto.builder()
+            .email("alekstest2@test.com")
+            .confirmEmail("alekstest2@test.com")
+            .firstName("Aleks2")
+            .lastName("Test2")
+            .password("mypass2")
+            .confirmPassword("mypass2")
             .build()
         );
     }
